2842171738236d98c1befa5fa04bcec745753e5f
restivo <restivo@umich.edu>
2026-07-13 22:57:11 -0400

    troubleshooting deployment changes

--- CS571_Web_Project/vite.config.js
+++ CS571_Web_Project/vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/CS571_Web_Project/',
+  base: '/',
   build: {
-    outDir: 'docs'
+    outDir: '../docs'
   }
 });
